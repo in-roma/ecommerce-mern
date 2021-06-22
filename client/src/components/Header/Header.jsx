11887bf91626c5cdcca9c1e0795e28134a133b88
Romain Grandjean <romain.grandjean@outlook.com>
2021-06-22 10:20:53 +0200

    header implementation

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import './header.scss';
 
 const Header = () => {
-	return <header className="navBar"></header>;
+	return (
+		<header className="navBar">
+			<div className="logo">PROSHOP</div>
+
+			<div className="menu">
+				<div className="menu-button cart">
+					<span>CART</span>
+				</div>
+				<div className="menu-button login">
+					<span>SIGN-IN</span>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
